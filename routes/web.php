6b94e47b164f70850c2957a99914c61b6fe87196
rilakkuma3 <xxxibgdrgn.s0818@i.softbank.jp>
2024-12-05 10:20:20 +0900

    管理者側の店舗管理機能

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
+use App\Http\Controllers\Admin\RestaurantController;
 use App\Http\Controllers\Admin\UserController;
 
 /*
@@ -27,5 +28,17 @@ require __DIR__.'/auth.php';
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
     Route::resource('users', Admin\UserController::class);
+    Route::resource('restaurants', RestaurantController::class);
 });
+
+Route::get('admin/restaurants/index', [Admin\RestaurantController::class, 'index'])->name('admin.restaurants.index');
+
+Route::get('admin/restaurants/create', [Admin\RestaurantController::class, 'create'])->name('admin.restaurants.create');
+
+Route::get('admin/restaurants/show={restaurant}', [Admin\RestaurantController::class, 'show'])->name('admin.restaurants.show');
+
+Route::resource('restaurants', RestaurantController::class, )->only('store', 'update', 'destroy');
+
+Route::get('admin/restaurants/edit', [Admin\RestaurantController::class, 'edit'])->name('admin.restaurants.edit');
+
 
